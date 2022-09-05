59ed2057509f0056314755c1000c93c41891b05b
kostatodor <109465276+kostatodor@users.noreply.github.com>
2022-09-05 17:42:48 +0300

    Урок №1 Задача№2

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+ int a, b;
+ 
+        Console.WriteLine("Введите два числа : ");
+ 
+        a = Convert.ToInt32(Console.ReadLine());
+ 
+        b = Convert.ToInt32(Console.ReadLine());
+ 
+        if (a > b)
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число", a);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число ", b);
+ 
+        }
+ 
+        Console.ReadLine();
